05d17b23855ddce47080e50b47fdebb4bc5eaa97
holdenparker <holdenparker55@gmail.com>
2024-12-12 22:14:19 -0500

    feat(02.2): Red-Nosed Reports part 2

--- 02/RedNosedReports.scala
+++ 02/RedNosedReports.scala
@@ -10,23 +10,64 @@ def RedNosedReports(): Unit = {
   _parseData(fileName, part1)
   printf("There are %d safe reports in part 1!\n", part1.safeReports)
 
+  val part2 = PartTwoReportAnalyzer()
+  _parseData(fileName, part2)
+  printf("There are %d safe reports in part 2!\n", part2.safeReports)
 }
 
 trait RedNoseReactorReportAnalyzer:
   def processLine(line: Array[Int]): Unit
 
-class PartOneReportAnalyzer extends RedNoseReactorReportAnalyzer {
+abstract class BaseReportAnalyzer {
+
+  def goingUp(line: Array[Int]) = line(0) < line(1)
+
+  def changeIsSafe(a: Int, b: Int, goingUp: Boolean): Boolean = {
+    val diff = a - b
+    if (diff.abs < 1 || diff.abs > 3) then return false
+    if ((diff < 0) != goingUp) then return false
+    return true
+  }
+}
+
+class PartOneReportAnalyzer extends BaseReportAnalyzer, RedNoseReactorReportAnalyzer {
   var safeReports = 0
 
   override def processLine(line: Array[Int]): Unit = {
-    val goingUp = if (line(0) > line(1)) false else true
+    val _goingUp = goingUp(line)
 
     boundary:
       for i <- 1 to line.length - 1 do
-        val diff = line(i - 1) - line(i)
-        if (diff.abs < 1 || diff.abs > 3) then break()
-        if ((diff < 0) != goingUp) then break()
+        if (!changeIsSafe(line(i - 1), line(i), _goingUp)) then break()
       safeReports += 1
+  }
+}
+
+class PartTwoReportAnalyzer extends BaseReportAnalyzer, RedNoseReactorReportAnalyzer {
+  var safeReports = 0
+
+  override def processLine(line: Array[Int]): Unit = {
+    val _goingUp = goingUp(line)
+
+    var isSafe = checkLineIsSafe(line)
+    boundary:
+      for i <- 0 to line.length - 1 do
+        if (isSafe) then break()
+        isSafe = checkLineIsSafe(line.slice(0, i) ++ line.slice(i + 1, line.length))
+    if (isSafe) then safeReports += 1
+  }
+
+  def checkLineIsSafe(line: Array[Int]): Boolean = {
+    val _goingUp = goingUp(line)
+    var result = true
+
+    boundary:
+      for i <- 1 to line.length - 1 do
+        if (!changeIsSafe(line(i - 1), line(i), _goingUp)) {
+          result = false
+          break()
+        }
+    return result;
   }
 }
 
